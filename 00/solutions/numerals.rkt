2c2bc8a4aa0f22c71e99aa3206cf71b2774215d7
Denis Belinov <belinovd@vmware.com>
2019-11-05 22:34:45 +0200

    Adding solutions for tasks 1,2,3

--- 00/solutions/numerals.rkt
+++ 00/solutions/numerals.rkt
@@ -7,17 +7,37 @@
          pred)
 
 (define zero (lambda (f v) v))
+(define one (lambda (f v) (f v)))
+(define two (lambda (f v) (f (f v))))
+
+(define (1+ n) (+ 1 n))
 
 (define (succ n)
   (lambda (f v)
     (f (n f v))))
 
-(define (from-numeral n) void)
+(define (from-numeral n) (n 1+ 0))
 
-(define (to-numeral n) void)
+(define (to-numeral n)
+  (if (= n 0)
+      (lambda (f v) v)
+      (succ (to-numeral (- n 1)))))
 
-(define (plus n m) void)
+(define (plus n m)
+  (lambda (f v) ((n succ m) f v)))
 
-(define (mult n m) void)
 
-(define (pred n) void)
+(define (mult n m)
+  (lambda (f v) ((n
+                  (lambda (numeral) (plus m numeral))
+                  zero)
+                 f v)))
+
+(define (pred n)
+  (lambda (f v) ((n
+                  (lambda (numeral)
+                    (if (equal? ((succ numeral) 1+ 0) (n 1+ 0))
+                        numeral
+                        (succ numeral)))
+                  zero)
+                 f v)))
--- 00/solutions/square-root.rkt
+++ 00/solutions/square-root.rkt
@@ -2,4 +2,18 @@
 
 (provide my-sqrt)
 
-(define (my-sqrt x) void)
+(define (my-sqrt x)
+  (define ITERATION_COUNT 10)
+  
+  (define (func y)
+    (- (* y y) x))
+
+  (define (func-der y)
+    (* 2.0 y))
+
+  (define (step s i)
+    (if (= i 0)
+        s
+        (step (- s (/ (func s) (func-der s))) (- i 1))))
+  
+  (step 10 ITERATION_COUNT))
