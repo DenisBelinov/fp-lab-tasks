abbaecd31967cec2cdca8b2166cba73470ec4c88
Denis Belinov <denisbelinov1@gmail.com>
2019-12-08 20:04:27 +0200

    Adding solutions for homework 2

--- 01/solutions/matrix.rkt
+++ 01/solutions/matrix.rkt
@@ -17,43 +17,87 @@
 ; the provide "exports" these functions
 
 ; 00.
-(define (all? p? xs) void)
+(define (all? p? xs)
+  (if (null? xs)
+      true
+      (and (p? (car xs)) (all? p? (cdr xs)))))
 
 ; 01.
-(define (any? p? xs) void)
+(define (any? p? xs)
+    (if (null? xs)
+      false
+      (or (p? (car xs)) (any? p? (cdr xs)))))
 
 ; 02.
-(define (concat xss) void)
+(define (concat xss)
+  (if (null? xss)
+      '()
+      (if (null? (car xss))
+          (concat (cdr xss))
+          (cons (caar xss) (concat (append (list (cdar xss)) (cdr xss)))))))
 
 ; 03.
-(define (rows xss) void)
+(define (rows xss) xss)
 
 ; 04.
-(define (cols xss) void)
+(define (cols xss)
+  (if (null? (car xss))
+      '()
+      (cons (map car xss) (cols (map cdr xss)))))
 
 ; 05.
-(define (matrix-ref xss i j) void)
+(define (matrix-ref xss i j)
+  (if (= i 0)
+      (list-ref (car xss) j)
+      (matrix-ref (cdr xss) (- i 1) j)))
 
 ; 06.
-(define (set xs i x) void)
+(define (set xs i x)
+  (if (null? xs)
+      '()
+      (if (= i 0)
+          (cons x (cdr xs))
+          (cons (car xs) (set (cdr xs) (- i 1) x)))))
 
 ; 07.
-(define (place xss i j x) void)
+(define (place xss i j x)
+  (if (= i 0)
+      (cons (set (car xss) j x) (cdr xss))
+      (cons (car xss) (place (cdr xss) (- i 1) j x))))
 
 ; 08.
-(define (diag xss) void)
+(define (diag xss)
+  (if (null? xss)
+      '()
+      (cons (caar xss) (diag (map cdr (cdr xss))))))
 
 ; 09.
-(define (diags xss) void)
+(define (flip xss)
+  (map reverse xss))
+
+(define (diags xss)
+  (cons (diag xss) (list (diag (flip xss)))))
 
 ; 10.
-(define (map-matrix f xss) void)
+(define (map-matrix f xss)
+  (if (null? xss)
+      '()
+      (cons (map f (car xss)) (map-matrix f (cdr xss)))))
 
 ; 11.
-(define (filter-matrix p? xss) void)
+(define (filter-matrix p? xss)
+    (if (null? xss)
+      '()
+      (cons (filter p? (car xss)) (filter-matrix p? (cdr xss)))))
 
 ; 12.
-(define (zip-with f xs ys) void)
+(define (zip-with f xs ys)
+  (if (or (null? xs) (null? ys))
+      '()
+      (cons (f (car xs) (car ys)) (zip-with f (cdr xs) (cdr ys)))))
 
 ; 13.
-(define (zip-matrix xss yss) void)
+(define (zip-matrix xss yss)
+  (if (or (null? xss) (null? yss))
+      '()
+      (cons (zip-with cons (car xss) (car yss)) (zip-matrix (cdr xss) (cdr yss)))))
--- 01/solutions/xno.rkt
+++ 01/solutions/xno.rkt
@@ -10,17 +10,118 @@
 
 ; winner implementation that only detects draws right now.
 ; Put your own implementation here!
+
+(define (get-winner xs)
+  (andmap (lambda (x)
+            (and (equal? x (car xs)) x))
+          xs))
+
+(define (horizontal-winner b)
+  (ormap get-winner b))
+
+(define (vertical-winner b)
+  (ormap get-winner (cols b)))
+
+(define (diag-winner b)
+  (ormap get-winner (diags b)))
+
+
 (define (winner b)
-  (if (andmap (lambda (xs) (andmap id xs)) b)
-      "D"
-      #f))
+  (define winner (or (horizontal-winner b) (vertical-winner b) (diag-winner b)))
+  (or 
+   winner
+   (if (andmap (lambda (xs) (andmap id xs)) b)
+       "D"
+       #f)))
+
 
 ; "Dumb" "AI", plays the "next" free spot, going left-to-right, top-to-bottom.
 ; Put your own implementation here!
+
+; returns a list of the indexes (ex. (0, 2)) which are false
+(define (get-free-places b)
+
+  (define (helper b i j)
+    
+    (define BOARD-SIZE (length b))
+    
+    (cond ((>= j BOARD-SIZE) (helper b (+ 1 i) 0))
+          ((>= i BOARD-SIZE) '())
+          (else (let* ((curr-el (matrix-ref b i j))
+                       (rec (helper b i (+ 1 j))))
+                  (if curr-el
+                      rec
+                      (cons (list i j) rec))))))
+
+  (helper b 0 0))
+
+(define (get-possible-future-boards b sign)
+  (map (lambda (p)
+         (place b (car p) (cadr p) sign))
+       (get-free-places b)))
+
+; returns assoc list with (board i j) where i and j are the coords of where the given sign was placed, used in play function
+(define (get-board-free-place-assoc b sign)
+  (define places (get-free-places b))
+  (define boards (get-possible-future-boards b sign))
+
+  (map cons boards places))
+
+
+; minimax algs
+(define (max-alg b)
+  (define w (winner b))
+  
+  (cond ((equal? w "X") 1)
+        ((equal? w "O") -1)
+        ((equal? w "D") 0)
+        (else (let ((possible-future-boards (get-possible-future-boards b "X")))
+                (apply max (map min-alg possible-future-boards))))))
+
+(define (min-alg b)
+  (define w (winner b))
+  
+  (cond ((equal? w "X") 1)
+        ((equal? w "O") -1)
+        ((equal? w "D") 0)
+        (else (let ((possible-future-boards (get-possible-future-boards b "O")))
+                (apply min (map max-alg possible-future-boards))))))
+
+
+; I know where I use these, no need for null checks trust me :)
+(define (max-first-elems xs1 xs2)
+  (if (>= (car xs1) (car xs2))
+      xs1
+      xs2))
+
+(define (min-first-elems xs1 xs2)
+  (if (<= (car xs1) (car xs2))
+      xs1
+      xs2))
+
+
+; these funcs are meant to be used on a bard->free-place assoc array's element
+(define (apply-max-alg xs)
+  (cons (max-alg (car xs)) (cdr xs)))
+
+(define (apply-min-alg xs)
+  (cons (min-alg (car xs)) (cdr xs)))
+
+
+; entrypoint funcs
+(define (maximise curr-board)
+  (let* ((actions (get-board-free-place-assoc curr-board "X"))
+         (move (cdr (foldr max-first-elems '(-10 100 100)
+                           (map apply-min-alg actions)))))
+    (cons (car move) (cadr move))))
+
+(define (minimize curr-board)
+  (let* ((actions (get-board-free-place-assoc curr-board "O"))
+         (move (cdr (foldr min-first-elems '(10 100 100)
+                     (map apply-max-alg actions)))))
+    (cons (car move) (cadr move))))
+
 (define (play curr-board curr-sign)
-  (define (helper i j)
-    (cond ((> i 2) #f)
-          ((> j 2) (helper (+ i 1) 0))
-          ((not (list-ref (list-ref curr-board i) j)) (cons i j))
-          (else (helper i (+ j 1)))))
-  (helper 0 0))
+  (if curr-sign
+      (maximise curr-board)
+      (minimize curr-board)))
