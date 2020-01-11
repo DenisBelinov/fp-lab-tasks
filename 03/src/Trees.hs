2adfb9129f82c35b5c9ca6b50d12bdd607710d86
Denis Belinov <denisbelinov1@gmail.com>
2020-01-11 17:54:04 +0200

    Add solutions for 03

--- 03/src/Trees.hs
+++ 03/src/Trees.hs
@@ -6,6 +6,9 @@ module Trees where
 import Prelude
 import Data.Monoid (Sum(..), All(..), Any(..), First(..))
 import Data.Maybe (isJust)
+import Data.Bifunctor
+
+import Instances (Dual(..))
 
 data Tree a
   = Empty
@@ -14,63 +17,79 @@ data Tree a
 
 instance Eq a => Eq (Tree a) where
   (==) :: Tree a -> Tree a -> Bool
-  (==) = undefined
+  Empty == Empty = True
+  _ == Empty = False
+  Empty == _ = False
+  (Node x t1 t2) == (Node x' t1' t2') = (x == x') && (t1 == t1') && (t2 == t2')
 
 insertOrdered :: Ord a => a -> Tree a -> Tree a
-insertOrdered = undefined
+insertOrdered x Empty = Node x Empty Empty
+insertOrdered x (Node a lt rt)
+  | x > a = Node a lt (insertOrdered x rt)
+  | otherwise = Node a (insertOrdered x lt) rt
 
 listToBST :: Ord a => [a] -> Tree a
-listToBST = undefined
+listToBST = foldr insertOrdered Empty
 
 isBST :: Ord a => Tree a -> Bool
-isBST = undefined
+isBST = between Bot Top
 
 -- idea for implementing isBST - delete if you don't want it
 data BotTop a = Bot | Val a | Top
   deriving (Show, Eq, Ord)
 
 between :: Ord a => BotTop a -> BotTop a -> Tree a -> Bool
-between = undefined
+between a b Empty = a <= b
+between a b (Node x lt rt) = a < Val x && b >= Val x && between a (Val x) lt && between (Val x) b rt
 
 findBST :: Ord a => a -> Tree a -> Bool
-findBST = undefined
+findBST _ Empty = False
+findBST a (Node x lt rt)
+  | x == a = True
+  | otherwise = findBST a lt || findBST a rt
 
 mapTree :: (a -> b) -> Tree a -> Tree b
-mapTree = undefined
+mapTree _ Empty = Empty
+mapTree f (Node x lt rt) = Node (f x) (mapTree f lt) (mapTree f rt)
 
 foldTree :: Monoid a => Tree a -> a
-foldTree = undefined
+foldTree Empty = mempty
+foldTree (Node x lt rt) = foldTree lt <> x <> foldTree rt
 
 foldMapTree :: Monoid b => (a -> b) -> Tree a -> b
-foldMapTree = undefined
+foldMapTree f = foldTree . mapTree f
 
 sumTree :: Num a => Tree a -> a
-sumTree = undefined
+sumTree = getSum . foldMapTree Sum
 
 allTree :: (a -> Bool) -> Tree a -> Bool
-allTree = undefined
+allTree p = getAll . foldMapTree (All . p)
 
 treeToList :: Tree a -> [a]
-treeToList = undefined
+treeToList = foldMapTree (:[])
 
 elemTree :: Eq a => a -> Tree a -> Bool
-elemTree = undefined
+elemTree x = getAny . foldMapTree (Any . (==x))
 
 onMaybe :: (a -> Bool) -> a -> Maybe a
 onMaybe p x = if p x then Just x else Nothing
 
 findPred :: (a -> Bool) -> Tree a -> Maybe a
-findPred = undefined
+findPred p = getFirst . foldMapTree (First . onMaybe p)
 
 findAll :: (a -> Bool) -> Tree a -> [a]
-findAll = undefined
+findAll p = foldMapTree (\x -> [x | p x])
 
 ifJust :: Maybe a -> (a -> Maybe b) -> Maybe b
 ifJust Nothing _ = Nothing
 ifJust (Just x) f = f x
 
 validateTree :: (a -> Maybe b) -> Tree a -> Maybe (Tree b)
-validateTree = undefined
+validateTree _ Empty = Just Empty
+validateTree f (Node v lt rt) = ifJust (validateTree f lt)
+                                       (\x -> ifJust (f v)
+                                                     (\y -> ifJust (validateTree f rt)
+                                                                   (Just . Node y x)))
 
 data Direction
   = L -- go left
@@ -78,7 +97,16 @@ data Direction
   deriving (Show, Eq)
 
 fetch :: [Direction] -> Tree a -> Maybe a
-fetch = undefined
+fetch _ Empty = Nothing
+fetch [] (Node x _ _) = Just x
+fetch (L:ds) (Node _ lt _) = fetch ds lt
+fetch (R:ds) (Node _ _ rt) = fetch ds rt
+
+mapDirections :: Tree a -> Tree (a, [Direction])
+mapDirections Empty = Empty
+mapDirections (Node x lt rt) = Node (x, []) newlt newrt
+  where newlt = mapTree (second ((:) L)) (mapDirections lt)
+        newrt = mapTree (second ((:) R)) (mapDirections rt)
 
 paths :: Tree a -> [(a, [Direction])]
-paths = undefined
+paths = treeToList . mapDirections
